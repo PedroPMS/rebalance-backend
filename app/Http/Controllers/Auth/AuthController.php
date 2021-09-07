00db821ee70622f0fd92c0891e5a93be9814fdef
Pedro Paulo Silva <p.paulo0512@gmail.com>
2021-09-07 11:27:10 -0300

    feature(Auth): implementing the auth routes

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Repositories\AuthRepository;
+use Illuminate\Http\JsonResponse;
+
+class AuthController extends Controller
+{
+    private AuthRepository $authRepository;
+
+    public function __construct(AuthRepository $authRepository)
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+        $this->authRepository = $authRepository;
+    }
+
+    public function login(LoginRequest $request): JsonResponse
+    {
+        if (!$token = auth()->attempt($request->validated())) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->createNewToken($token);
+    }
+
+    public function register(RegisterRequest $request): JsonResponse
+    {
+        return $this->authRepository->register($request);
+    }
+
+    public function logout(): JsonResponse
+    {
+        auth()->logout();
+
+        return response()->json(['message' => 'User successfully signed out']);
+    }
+
+    public function refresh(): JsonResponse
+    {
+        return $this->createNewToken(auth()->refresh());
+    }
+
+    public function userProfile(): JsonResponse
+    {
+        return response()->json(auth()->user());
+    }
+
+    protected function createNewToken(string $token): JsonResponse
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type'   => 'bearer',
+            'expires_in'   => auth()->factory()->getTTL() * 60,
+            'user'         => auth()->user()
+        ]);
+    }
+
+}
